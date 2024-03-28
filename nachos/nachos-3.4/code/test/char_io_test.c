2ee398e2ede5bae0e0c968a551a5b42be2a1637a
Nguyen Gia Huy <nghuy22@clc.fitus.edu.vn>
2024-03-28 18:23:02 +0700

    [code/test] update char_io_test

--- nachos/nachos-3.4/code/test/char_io_test.c
+++ nachos/nachos-3.4/code/test/char_io_test.c
@@ -1,6 +1,40 @@
 #include "syscall.h"
 
+#define NULL 0
+#define BACKSPACE 8
+#define ESC 27
+#define SPACE 32
+#define DELETE 127
+#define TAB 9
+
 int main()
 {
-    PrintChar(ReadChar());
+    char c;
+    PrintString("*====================================*\n");
+    PrintString("| Welcome to the Char I/O Test Suite |\n");
+    PrintString("*====================================*\n");
+
+    PrintString("Enter a character: ");
+    c = ReadChar();
+    PrintString("You entered: ");
+    switch (c)
+    {
+    case NULL:
+        PrintString("NULL\n");
+        break;
+        break;
+    case ESC:
+        PrintString("ESC\n");
+        break;
+    case SPACE:
+        PrintString("SPACE\n");
+        break;
+    case TAB:
+        PrintString("TAB\n");
+        break;
+    default:
+        PrintChar(c);
+        break;
+    }
+    PrintChar('\n');
 }

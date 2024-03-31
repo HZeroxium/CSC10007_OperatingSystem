6a567c88787f0d8aecf86f6e455c414e8ef7d289
Nguyen Gia Huy <nghuy22@clc.fitus.edu.vn>
2024-03-31 19:20:39 +0700

    [code/test] update help

--- nachos/nachos-3.4/code/test/help.c
+++ nachos/nachos-3.4/code/test/help.c
@@ -6,7 +6,7 @@ int main()
 {
     int openFileId;           // File descriptor for the file to be read
     char buffer[BUFFER_SIZE]; // Buffer to store the content of the file
-
+    int result = 0;           // Result of the read operation
     PrintString("*==========================================================*\n");
     PrintString("|      Help command for Nachos file system (mota.txt)      |\n");
     PrintString("*==========================================================*\n");
@@ -16,15 +16,33 @@ int main()
     openFileId = Open("mota.txt", 1); // Open the file to read
     if (openFileId != -1)             // If the file is opened successfully
     {
-        Read(buffer, BUFFER_SIZE, openFileId);   // Read the content of the file
-        PrintString(buffer);                     // Print the content of the file
-        Close(openFileId);                       // Close the file
-        PrintString("File read successfully\n"); // Print success message
-        return 0;
+        result = Read(buffer, BUFFER_SIZE, openFileId); // Read the content of the file
+        if (result == -1)
+        {
+            PrintString("!!!ERROR: File is not found or id is out of range or the file is not opened in read mode (stdout)\n");
+        }
+        else if (result == -2)
+        {
+            PrintString("!!!ERROR: File is empty (EOF)\n");
+        }
+        else if (result == -3)
+        {
+            PrintString("!!!ERROR: Unknown error occurs\n");
+        }
+        else
+        {
+            PrintString(buffer); // Print the content of the file
+        }
+        Close(openFileId); // Close the file
     }
     else // If the file open failed
     {
         PrintString("Can't open file\n"); // Print error message
-        return 1;
     }
+
+    PrintString("*=============================================*\n");
+    PrintString("| Thank you for using this program in Nachos! |\n");
+    PrintString("*=============================================*\n");
+
+    return 0;
 }

24a5787dde600e12a4f192df0f4b01c87a3de15b
Nguyen Gia Huy <nghuy22@clc.fitus.edu.vn>
2024-03-31 19:21:37 +0700

    [code/test] update mergesort

--- nachos/nachos-3.4/code/test/mergesort.c
+++ nachos/nachos-3.4/code/test/mergesort.c
@@ -1,6 +1,146 @@
 #include "syscall.h"
 
+#define MAX_SIZE (20)
+
 int main()
 {
-    Halt();
+    float *a[MAX_SIZE];    // array of float pointers
+    float *b[MAX_SIZE];    // temporary array of float pointers for merging (left half)
+    float *c[MAX_SIZE];    // temporary array of float pointers for merging (right half)
+    int n;                 // number of elements in the array
+    int i, j, k;           // loop variables
+    int nb;                // number of elements in b (left half)
+    int nc;                // number of elements in c
+    int size;              // size of the sub array to be sorted
+    int left;              // left index of the sub array to be sorted
+    int mid;               // middle index of the sub array to be sorted
+    int right;             // right index of the sub array to be sorted
+    int id;                // index of the sub array to be sorted
+    int ib;                // index of the element in b
+    int ic;                // index of the element in c
+    OpenFileId openFileId; // File descriptor for the file to be written
+    char temp[8];          // Temporary buffer for writing float to file
+
+    // PrintString("*====================================================*\n");
+    // PrintString("| Welcome to the Merge Sort Float program in Nachos! |\n");
+    // PrintString("*====================================================*\n");
+
+    // Read n, the number of elements in the array
+    do
+    {
+        PrintString("Enter number of elements in the array (n, 1 <= n < 100): ");
+        n = ReadInt();
+        if (n <= 0 || n > 100)
+            PrintString("n must be an integer between 1 and 100 (inclusive), please try again\n");
+    } while (n <= 0 || n > 100);
+    // Read the elements of the array
+
+    for (i = 0; i < n; i++)
+    {
+        PrintString("+ Enter float element [");
+        PrintInt(i);
+        PrintString("]: ");
+        a[i] = ReadFloat();
+    }
+
+    PrintString("\n-> Initial array: [");
+    for (i = 0; i < n - 1; i++)
+    {
+        PrintFloat(a[i]);
+        PrintString(", ");
+    }
+    if (n > 0)
+        PrintFloat(a[n - 1]);
+    PrintString("]\n");
+
+    // for each size is doubled
+    //     meaning we sort a pair of sub arrays of size 1, 2, 4, 8, 16, ...
+    for (size = 1; size <= n; size = size * 2)
+    {
+        // for each pair of sub arrays of given size
+        //     the next pair should starts after size * 2 elements
+        for (id = 0; id < n; id += size * 2)
+        {
+            // we currently sort a[left..right) based on a[left..mid) and a[mid..right)
+            left = id;
+            mid = (n < id + size ? n : id + size);
+            right = (n < id + size * 2 ? n : id + size * 2);
+
+            // copy a[left..mid) into b[0..size/2)
+            nb = 0;
+            for (i = left; i < mid; ++i)
+            {
+                b[nb++] = a[i];
+            }
+            // copy a[mid..right) into c[0..size/2)
+            nc = 0;
+            for (i = mid; i < right; ++i)
+            {
+                c[nc++] = a[i];
+            }
+
+            // merge a[left..mid) and a[mid..right) into a[left..right)
+            for (ib = 0, ic = 0; ib < nb && ic < nc;)
+            {
+                if (CompareFloat(b[ib], c[ic]) <= 0)
+                {
+                    a[left++] = b[ib++];
+                }
+                else
+                {
+                    a[left++] = c[ic++];
+                }
+            }
+            while (ib < nb)
+            {
+                a[left++] = b[ib++];
+            }
+            while (ic < nc)
+            {
+                a[left++] = c[ic++];
+            }
+        }
+    }
+
+    PrintString("=> Sorted array : [");
+    for (i = 0; i < n - 1; i++)
+    {
+        PrintFloat(a[i]);
+        PrintString(", ");
+    }
+    if (n > 0)
+        PrintFloat(a[n - 1]);
+    PrintString("]\n\n");
+
+    openFileId = Open("mergesort.txt", 0);
+    if (openFileId != -1)
+    {
+        for (i = 0; i < n; i++)
+        {
+            FloatToString(a[i], temp);
+            Write(temp, 8, openFileId);
+            Write("\n", 1, openFileId);
+        }
+        Close(openFileId);
+        PrintString("-> Write sorted array to file mergesort.txt successfully\n\n");
+    }
+    else
+    {
+        PrintString("Cannot open file mergesort.txt\n");
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        FreeFloat(a[i]);
+    }
+
+    PrintString("*=============================================*\n");
+    PrintString("| Thank you for using this program in Nachos! |\n");
+    PrintString("*=============================================*\n");
+
+    PrintChar('\n');
+
+    return 0;
 }
+
+#undef MAX_SIZE

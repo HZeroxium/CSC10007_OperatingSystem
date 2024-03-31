0870f2c3b89b855470de30ef2c687dd280a1ad4c
Nguyen Gia Huy <nghuy22@clc.fitus.edu.vn>
2024-03-31 19:20:30 +0700

    [code/test] update float_io_test

--- nachos/nachos-3.4/code/test/float_io_test.c
+++ nachos/nachos-3.4/code/test/float_io_test.c
@@ -2,5 +2,17 @@
 
 int main()
 {
-    PrintFloat(ReadFloat());
+    float *pf;
+    float *f;
+
+    PrintString("*=====================================*\n");
+    PrintString("| Welcome to the Float I/O Test Suite |\n");
+    PrintString("*=====================================*\n");
+    PrintString("\nEnter a float: ");
+    pf = ReadFloat();
+    f = pf;
+    PrintString("-> Float that you entered: ");
+    PrintFloat(pf);
+
+    PrintString("\nExiting Float I/O Test Suite\n\n");
 }
